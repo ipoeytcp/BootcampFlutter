bcc96a20fe63bb232ad1b73c87d1ba9912a9044b
Syaiful hadi <ipoey.design@gmail.com>
2024-03-26 15:13:31 +0700

    Tugas-2-Dart

---
+++ Tugas-2A-TypeData/materi_dart.dart
@@ -0,0 +1,17 @@
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   print("masukan password");
+//   String inputText = stdin.readLineSync()!;
+//   print("password :"+ inputText);
+// }
+
+void main(List<String> args) {
+  var name = "Ipoey" ;
+  var angka = 12 ; 
+  var angkabulat = 3.14 ;
+
+    print(name);
+    print(angka);
+    print(angkabulat);
+}
---
+++ Tugas-2A-TypeData/type.dart
@@ -0,0 +1,55 @@
+// SOAL NO 1
+// void main(List<String> args) {
+//   var word = "DART";
+//   var second = "IS";
+//   var third = "AWESOME";
+//   var fourth = "AND";
+//   var fifth = "I";
+//   var sixth = "LOVE";
+//   var seventh = "IT!";
+
+//   print(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
+// }
+
+// SOAL NO 2
+/* void main(List<String> args) {
+  var sentence = "I am going to be Flutter Developer";
+  var firstWord = sentence[0] ;
+  var secondWord = sentence[2] + sentence[3] ;
+  var thrirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9] + sentence[10] ;
+  var fourthWord = sentence[11] + sentence[12] ;
+  var fifthWord = sentence[14] + sentence[15] ;
+  var sixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23] ;
+  var sevenththWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] + sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33]  ;
+
+  print('First Word: ' + firstWord);
+  print('Second Word: ' + secondWord);
+  print('Third Word: ' + thrirdWord);
+  print('Fourth Word: ' + fourthWord);
+  print('Fifth Word: ' + fifthWord);
+  print('Sixth Word: ' + sixthWord);
+  print('Seventh Word: ' + sevenththWord);
+} */
+
+// SOAL NO 3
+/* import 'dart:io';
+
+void main(List<String> args) {
+  print("Masukan Nama Depan");
+  String? inputText =  stdin.readLineSync()!;
+  print("Masukan Nama Belakang");
+  String? inputText2 =  stdin.readLineSync()!;
+  print("Nama Lengkap Anda Adalah: ${inputText} ${inputText2}");
+} */
+
+//SOAL NO 4
+/* void main(List<String> args) {
+  var a = 5 ;
+  var b = 10 ;
+
+  print("perkalian : ${a * b}" );
+  print("pembagian : ${a / b}" );
+  print("penambahan : ${a + b}" );
+  print("pengurangan : ${a - b}" );
+} */
+
