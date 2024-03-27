8894cafcc637a6de6bc7bc4794629917337e44b1
Syaiful hadi <ipoey.design@gmail.com>
2024-03-27 19:14:40 +0700

    Tugas 3A

---
+++ Tugas-3/Tugas-3A-Looping/lopping.dart
@@ -0,0 +1,81 @@
+
+//SOAL NO 1
+// void main()
+
+// {
+//    print("LOOPING PERTAMA");
+// {
+//   var deret = 10;
+//   var jumlah = 0;
+//   while (deret > 0) 
+ 
+//   {
+//     // Loop akan terus berjalan selama nilai deret masih di atas 0
+//     jumlah += 2; // Menambahkan nilai variable jumlah dengan angka deret
+//     deret--; // Mengubah nilai deret dengan mengurangi 1
+//     print( jumlah.toString()+ ' - I love coding');
+//   }
+
+//  // print(jumlah);
+// }
+//  print("LOOPING KEDUA");
+// {
+//   var deret = 10;
+//   var jumlah = 22;
+//   while (deret >= 1) 
+ 
+//   {
+//     // Loop akan terus berjalan selama nilai deret masih di atas 0
+//     jumlah -= 2; // Menambahkan nilai variable jumlah dengan angka deret
+//     deret--; // Mengubah nilai deret dengan mengurangi 1
+//     print( jumlah.toString()+ ' - I will become a mobile developer');
+//   }
+
+//  //print(jumlah);
+// }
+
+// }
+
+//SOAL NO 2
+// void main(List<String> args) 
+// {
+//   for (var deret = 1; deret <= 20; deret += 1)
+//   {
+//     if (deret.isOdd && deret % 3 != 0 )
+//     {
+//         print(deret.toString()+ ' - Santai');
+//     } 
+//     else if (deret.isEven)
+//     {
+//       print(deret.toString()+ ' - Berkualitas');
+//     }
+//     else if (deret % 3 == 0 && deret != deret.isEven) 
+//     {
+//       print(deret.toString()+ ' - I Love Coding');
+//     } 
+//     else 
+//     {
+//       print("BELOM");
+//     }
+    
+//   }
+// }
+
+// SOAL NO 3
+// void main(List<String> args) {
+//   for (int counter = 0; counter < 4; counter += 1){
+//     print("########");
+//   }
+// }
+
+//SOAL NO 4
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   for (int  a = 1; a <= 7; a++){
+//       for (int b = 1; b <= a; b++ ){
+//         stdout.write('#');
+//       }
+//     print('');
+//   }
+// }
---
+++ Tugas-3/Tugas-3A-Looping/materi_looping.dart
@@ -0,0 +1,35 @@
+void main()
+
+{
+   print("LOOPING PERTAMA");
+{
+  var deret = 10;
+  var jumlah = 0;
+  while (deret > 0) 
+ 
+  {
+    // Loop akan terus berjalan selama nilai deret masih di atas 0
+    jumlah += 2; // Menambahkan nilai variable jumlah dengan angka deret
+    deret--; // Mengubah nilai deret dengan mengurangi 1
+    print( jumlah.toString()+ ' - I love coding');
+  }
+
+ // print(jumlah);
+}
+ print("LOOPING KEDUA");
+{
+  var deret = 10;
+  var jumlah = 22;
+  while (deret >= 1) 
+ 
+  {
+    // Loop akan terus berjalan selama nilai deret masih di atas 0
+    jumlah -= 2; // Menambahkan nilai variable jumlah dengan angka deret
+    deret--; // Mengubah nilai deret dengan mengurangi 1
+    print( jumlah.toString()+ ' - I will become a mobile developer');
+  }
+
+ //print(jumlah);
+}
+
+}
