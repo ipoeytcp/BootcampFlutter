26c338366dd281fbb1d6ff3a56da94e28c20ecd6
Syaiful hadi <ipoey.design@gmail.com>
2024-03-27 20:01:17 +0700

    Tuugas 3B

---
+++ Tugas-3/Tugas-3B-Function/function.dart
@@ -0,0 +1,47 @@
+// SOAL NO 1
+// void main(List<String> args) {
+//   teriak();
+// }
+
+// teriak(){
+//   print("Halo Sanbers!");
+// }
+
+//SOAL NO 2
+// void main(List<String> args) {
+//   kalikan(12, 4);
+// }
+//   kalikan(x, y) {
+//   int hasilKali = x * y;
+//   print(hasilKali);
+// }
+
+// SOAL NO 3
+// void main(List<String> args) {
+// introduce();
+// }
+// introduce() {
+// final name = "Achmad";
+// final age = 30;
+// final address = "Jln. Malioboro, Yogyakarta";
+// final hobby = "Gaming!";
+// final introduce =
+// "Nama saya adalah $name, usia saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby";
+// print(introduce);
+// }
+
+// SOAL NO 4
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   var num, faktorial = 1;
+//   stdout.write("Silahkan Masukan Angka : ");
+//   String? ss = stdin.readLineSync();
+//   if (ss != null) {
+//     int num = int.parse(ss);
+//     for (int i = 1; i <= num; i++) {
+//       faktorial = faktorial * i;
+//     }
+//     print("Faktorial dari angka $num adalah $faktorial");
+//   }
+// }
