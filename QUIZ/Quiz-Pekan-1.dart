c9ffc80602d1f1f4ad0dcb336bbdd5edebd30513
Syaiful hadi <ipoey.design@gmail.com>
2024-03-30 12:25:37 +0700

    Quiz Pekan 1

---
+++ QUIZ/Quiz-Pekan-1.dart
@@ -0,0 +1,40 @@
+// // void main(List<String> args) {
+// //   var total = 5;
+// //   var output = "";
+
+// //   for (var i=0; i<= total; i++){
+// //     for (var j = 1; j<= i; j++){
+// //       output += j.toString();
+// //     }
+// //     print(output);
+// //     output;
+// //   }
+
+// // }
+
+// // void main(List<String> args) {
+// //   printMessage(" Hello, World", "hilmy", "Mike");
+// // }
+
+// // printMessage(String message, String from, [String to = 'Unknown']){
+// //   print(' "${message}", from ${from} to ${to}');
+// // }
+
+// // void main(List<String> args) {
+// //   var jumlah = 0;
+// //   for (var i=0; i<=5; i++){
+// //     jumlah += i;
+// //   }
+// //   print(jumlah);
+// // }
+
+
+//   double sum(double a, double b){
+//     return a+b;
+//   }
+//   double square (double x) => x * x;
+
+//  void main(List<String> args) {
+//     print(sum(20.0, 30.0));
+//     print(square(7.0));
+//  }
